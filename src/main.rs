dd9e4c0eadeab2cd247fa0958a93bdc9a2022a1f
Emin Arıcan <emin.arican@outlook.com>
2021-07-06 12:45:33 +0300

    replace tui with cli

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,62 @@
-use crossterm::event::KeyCode;
+mod storage;
 
-mod input;
-mod ui;
-
-struct Passman {
-    input: input::Input,
-    ui: ui::Ui,
-}
-
-impl Passman {
-    fn new() -> Passman {
-        Passman {
-            input: input::new(),
-            ui: ui::new()
-        }
-    }
-
-    fn start(mut self) {
-        loop {
-            self.ui.render();
-            if let Some(key) = self.input.handle() {
-                match key {
-                    KeyCode::Esc => break,
-                    _ => {}
-                }
-            }
-        }
-        println!("bye!")
-    }
-}
+use clap::{App, Arg};
 
 fn main() {
-    let passman = Passman::new();
-    passman.start()
+    let secret = Arg::new("secret")
+        .about("Sets secret")
+        .value_name("password")
+        .takes_value(true)
+        .required(true)
+        .long("secret")
+        .short('s');
+    let provider = Arg::new("provider")
+        .about("Sets provider")
+        .value_name("provider")
+        .required(true);
+    let matches = App::new("Passman")
+        .version("0.1.0")
+        .about("Personal password manager")
+        .subcommand(App::new("new")
+            .about("Creates new password file")
+            .arg(Arg::new("secret")
+                .about("Sets secret")
+                .value_name("password")
+                .required(true)
+            )
+        )
+        .subcommand(App::new("set")
+            .about("Sets a new password")
+            .arg(&secret)
+            .arg(&provider)
+            .arg(Arg::new("value")
+                .about("Sets value")
+                .value_name("value")
+                .required(true)
+            )
+        )
+        .subcommand(App::new("get")
+            .about("Gets a password")
+            .arg(&secret)
+            .arg(&provider)
+        )
+        .subcommand(App::new("gen")
+            .about("Generates a password")
+            .arg(&secret)
+            .arg(&provider)
+        )
+        .get_matches();
+    if let Some(matches) = matches.subcommand_matches("new") {
+        let password = matches.value_of("secret").unwrap();
+        println!("new {}", password)
+    } else if let Some(matches) = matches.subcommand_matches("set") {
+        let password = matches.value_of("secret").unwrap();
+        println!("set {}", password)
+    } else if let Some(matches) = matches.subcommand_matches("get") {
+        let password = matches.value_of("secret").unwrap();
+        println!("get {}", password)
+    } else if let Some(matches) = matches.subcommand_matches("gen") {
+        let password = matches.value_of("secret").unwrap();
+        println!("gen {}", password)
+    }
 }
